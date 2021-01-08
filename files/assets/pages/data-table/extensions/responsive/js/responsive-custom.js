0f0e7a76e7512cfff29327a6e912b81547e03910
Sameer Khalid <54281599+sameerkhalid007@users.noreply.github.com>
2021-01-08 18:42:40 +0530

    Add files via upload

---
+++ files/assets/pages/data-table/extensions/responsive/js/responsive-custom.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('#res-config').DataTable({responsive:true});var newcs=$('#new-cons').DataTable();new $.fn.dataTable.Responsive(newcs);$('#show-hide-res').DataTable({responsive:{details:{display:$.fn.dataTable.Responsive.display.childRowImmediate,type:''}}});});
